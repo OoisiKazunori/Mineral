3595a5d8186d881d49deac38113409c60acaa2e5
OoishiKazunori <cinc.higahi@gmail.com>
2023-10-04 03:14:39 +0900

    各GBufferの出力の値修正

--- Resource/ShaderFiles/ShaderFile/Sprite.hlsl
+++ Resource/ShaderFiles/ShaderFile/Sprite.hlsl
@@ -77,10 +77,10 @@ struct GBufferOutput
 GBufferOutput InstancePSMain(VSOutputColor input) : SV_TARGET
 {
     GBufferOutput output;
-    output.albedo = float4(1,1,1,1);
-    output.normal = float4(0,0,0,0);
-    output.metalnessRoughness = float4(0,0,0,0);
-    output.emissive = float4(0,0,0,0);
-    output.world = float4(0,0,0,0);
+    output.albedo = float4(tex.Sample(smp, input.uv)) * input.color;
+    output.normal = float4(-1,-1,-1,1);
+    output.metalnessRoughness = float4(0,0,0,1);
+    output.emissive = float4(1,1,1,1);
+    output.world = float4(0,0,0,1);
     return output;
 }
